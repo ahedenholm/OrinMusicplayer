2cf0653a7468d90d7b6eeca97e4139e40a0400a3
Anders Hedenholm <bocheadphase@gmail.com>
2017-09-13 22:18:52 +0200

    relative layout added to scale better on higher resolutions

--- app/src/main/java/com/orin/anders/orinmusicplayer/Main_Activity.java
+++ app/src/main/java/com/orin/anders/orinmusicplayer/Main_Activity.java
@@ -40,7 +40,7 @@ public class Main_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_2);
 
         Main_ActivityHelper.activity = this;
         Main_ActivityHelper.context = this;
