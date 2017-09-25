f760e14f36b303d430325c1d3046f1f30867d5fc
Anders Hedenholm <bocheadphase@gmail.com>
2017-09-26 01:03:14 +0200

    layouts and class added for artist view

---
+++ app/src/main/java/com/orin/anders/orinmusicplayer/activities/Artist_Activity.java
@@ -0,0 +1,17 @@
+package com.orin.anders.orinmusicplayer.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.orin.anders.orinmusicplayer.R;
+
+public class Artist_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist);
+
+    }
+}
